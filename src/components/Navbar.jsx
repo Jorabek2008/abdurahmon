26998160e7a377ae6adad991364783afec75a548
Jo'rabek <jorabekashurqulov400@gmail.com>
2024-04-20 22:52:42 +0500

    the end

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import cryptoLogo from "/media.png";
 import { LINK } from "./../mock/link";
 import { Link } from "react-scroll";
 import { FaBars } from "react-icons/fa6";
+import { IoMdClose } from "react-icons/io";
 
 const Navbar = () => {
   const [active, setActive] = useState("Home");
@@ -43,13 +44,49 @@ const Navbar = () => {
           Get Started
         </button>
         <div className="md:hidden">
-            <FaBars className="text-white text-[25px]" />
+          <FaBars
+            onClick={() => setOpen((a) => !a)}
+            className="text-white text-[25px]"
+          />
         </div>
 
-
-
-        <div className={`fixed top-0 left-0 w-full bg-[#151515] h-screen`}>
-
+        <div
+          className={`fixed top-0 ${
+            open ? "left-0" : "left-[-800px]"
+          } transition-all w-full bg-[#151515] md:hidden h-screen`}
+        >
+          <div className="mx-auto sm:w-[600px] md:w-[700px] py-[25px]">
+            <div className="w-full">
+              <IoMdClose
+                size={30}
+                className="text-[white] ml-auto"
+                onClick={() => setOpen((a) => !a)}
+              />
+            </div>
+            <ul className="md:hidden mt-10 gap-[20px]">
+              {LINK.map((item) => (
+                <Link
+                  key={item.id}
+                  to={item.path}
+                  spy={true}
+                  smooth={true}
+                  offset={-100}
+                  duration={1000}
+                  onClick={() => setActive(item.title)}
+                  className={` transition-all ${
+                    active == item.title ? "text-[#00C2FF]" : "text-white"
+                  } cursor-pointer w-full block text-center mt-5 text-[20px]`}
+                >
+                  <p onClick={() => setOpen((a) => !a)}>{item.title}</p>
+                </Link>
+              ))}
+            </ul>
+            <div className="text-center mt-9" onClick={() => setOpen(a => !a)}>
+              <button className="bg-[#70022A] md:hidden px-[20px] py-[10px] rounded-md hover:bg-white hover:text-[#70022A] transition-all ease-in text-white font-semibold">
+                Get Started
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
